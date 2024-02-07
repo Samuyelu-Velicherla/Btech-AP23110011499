d39f8df94e328541bbaf7696f387c6d26c20370c
Samuyelu-Velicherla <157694107+Samuyelu-Velicherla@users.noreply.github.com>
2024-02-07 15:47:24 +0530

    Update Labtask2_1.c

--- Labtask2_1.c
+++ Labtask2_1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+    //Write a program in C to store elements in an array and print them.
     int arr[5]={1,2,3,4,5};
     printf("---output---\n");
     for(int i=0;i<5;i++)
